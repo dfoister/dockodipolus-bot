0500b77525097aeaeac0af39298266f62bbd64f6
dfoister <devonfoister@gmail.com>
2026-04-28 14:45:45 +0100

    standings command

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const commands = [
   new SlashCommandBuilder()
     .setName('get-matches')
     .setDescription('Get matches'),
+  new SlashCommandBuilder()
+    .setName('standings')
+    .setDescription('See current standings'),
 ].map(command => command.toJSON());
 
 
@@ -77,6 +80,42 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply({
       embeds,
     });
+  }
+
+  if (interaction.commandName === 'standings') {
+
+    const response = await fetch('https://api.ukicircuit.com/leagues/2c55c3f9-c491-44d6-94e3-1b87d0be8650');
+    const standingsJson = await response.json();
+    const standings = standingsJson[0];
+
+    const standingsText = standings.map(team => {
+      const rank = String(team.rank).padStart(2);
+      const name = team.name.padEnd(20);
+      const record = `${team.wins}-${team.losses}`.padEnd(5);
+
+      const rd = (
+        Number(team.round_difference) > 0
+          ? `+${team.round_difference}`
+          : team.round_difference
+      ).padStart(4);
+
+      return `${rank}  ${name}  ${record}  ${rd}`;
+    }).join('\n');
+
+    const embed = new EmbedBuilder()
+      .setColor(0x5865F2)
+      .setTitle('UKIC Rising Standings')
+      .setURL('https://ukicircuit.com/leagues/2c55c3f9-c491-44d6-94e3-1b87d0be8650')
+      .setDescription(
+        '```' +
+        '\n    TEAM                  W-L      RD\n' +
+        standingsText +
+        '\n```'
+    );
+
+  await interaction.reply({
+    embeds: [embed],
+  });
   }
 });
 
